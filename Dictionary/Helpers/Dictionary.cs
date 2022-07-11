eab3201b3ea92f909e6362db7744cc540af10d24
Wiseman.Magagula <wiseman.magagula@investec.co.za>
2022-07-11 08:23:36 +0200

    Added few word types and words

--- Dictionary/Helpers/Dictionary.cs
+++ Dictionary/Helpers/Dictionary.cs
@@ -4,23 +4,64 @@
     {
         public static List<string> WordTypes = new List<string>()
         {
-            "Nonus",
-            "Pronouns",
-            "Verbs"
+            "Noun",
+            "Pronoun",
+            "Verb",
+            "Adjective",
+            "Adverb",
+            "Preposition",
+            "Conjuction",
+            "Determiner",
+            "Exclamation"
         };
 
         public static Dictionary<string, List<string>> WordsByType = new Dictionary<string, List<string>>()
         {
-            {"Nonus", new List<string>()
+            {"Noun", new List<string>()
                 {
                     "Computer", "Light", "Chair"
                 }
             },
-            {"Verbs", new List<string>()
+            {"Verb", new List<string>()
                 {
                     "Eat", "Sit", "Type"
                 }
-            }
+            },
+            {"Pronoun", new List<string>()
+                {
+                "He", "She"
+                }
+             },
+            {"Adjective", new List<string>()
+                {
+                "Sweet", "Hot", "technical"
+                }
+             },
+             {"Adverb", new List<string>()
+                {
+                "Slowly", "Rapidly", "Badly"
+                }
+             },
+             {"Preposition", new List<string>()
+                {
+                "off the", "in front", "to"
+                }
+             },
+             {"Conjuction", new List<string>()
+                {
+                "Or", "So", "Because"
+                }
+             },
+             {"Determiner", new List<string>()
+                {
+                "The", "This", "That"
+                }
+             },
+             {"Exclamation", new List<string>()
+                {
+                "Wow", "Oh", "Hooray"
+                }
+             }
         };
     }
 }
